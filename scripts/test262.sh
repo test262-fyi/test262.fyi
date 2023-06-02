59bb5bb1757e92fe7bd17cec7354436c33a826cd
CanadaHonk <oj@oojmed.com>
2023-06-02 23:16:50 +0100

    debugging

--- scripts/test262.sh
+++ scripts/test262.sh
@@ -7,4 +7,5 @@ cd test262
 # install test262 harness (https://github.com/bterlson/test262-harness)
 npm install -g test262-harness
 
+echo test/**/*.js
 test262-harness --host-type="$1" --host-path="$2" --reporter=json --reporter-keys=file,result --threads=4 test/**/*.js > ../results.json
