1b8294f5bd4da38006d04eac130babe7e780660b
CanadaHonk <oj@oojmed.com>
2023-06-03 09:41:13 +0100

    add hermes and chakracore

---
+++ scripts/engines/chakra.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./scripts/install/jsvu.sh shakra
+./scripts/test262.sh ch "${HOME}/.jsvu/bin/ch"
---
+++ scripts/engines/hermes.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./scripts/install/jsvu.sh hermes
+./scripts/test262.sh hermes "${HOME}/.jsvu/bin/hermes"
