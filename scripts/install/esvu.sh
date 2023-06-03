849e58d344fe4fb44a9ee78e1c8d3dc680c5c911
CanadaHonk <oj@oojmed.com>
2023-06-03 13:44:26 +0100

    scripts: install esvu from gh not npm

--- scripts/install/esvu.sh
+++ scripts/install/esvu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install esvu as extra/missing installer (https://github.com/devsnek/esvu)
-npm install -g esvu
+npm install -g github:devsnek/esvu
 
 # install given engine
 esvu --engines=$1
