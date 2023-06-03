8696d3c3b13042e8f9e9bf6890d4a16d36708d71
CanadaHonk <oj@oojmed.com>
2023-06-03 23:54:09 +0100

    scripts: use our fork of test262-harness

--- scripts/test262.sh
+++ scripts/test262.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
 
-cat "$HOME/.jsvu/status.json" || true
-cat "$HOME/.esvu/status.json" || true
-
-exit
-
 # clone test262
 git clone https://github.com/tc39/test262.git --depth 1
 cd test262
 
-# install test262 harness (https://github.com/bterlson/test262-harness)
-npm install -g test262-harness
+# install test262 harness (our fork - https://github.com/CanadaHonk/test262-harness)
+npm install -g github:CanadaHonk/test262-harness
 
 echo running test262...
 test262-harness --host-type="$1" --host-path="$2" --reporter=json --reporter-keys=file,result --threads=4 "test/**/*.js" > ../results.json
