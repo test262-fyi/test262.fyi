7a7cfcd58550807c17ec7ad57afaf0b404d59d0d
CanadaHonk <oj@oojmed.com>
2023-06-04 20:12:44 +0100

    hermes: fix extraction

--- scripts/engines/hermes.sh
+++ scripts/engines/hermes.sh
@@ -3,6 +3,6 @@
 node scripts/install/circle.js facebook/hermes main build linux "output/hermes-cli-linux.tar.gz" hermes.tar.gz
 
 mkdir hermes
-tar -zxf hermes.tar.gz --directory "hermes"
+tar -zxf hermes.tar.gz -C "hermes"
 
 ./scripts/test262.sh hermes "$PWD/hermes/hermes" 24
