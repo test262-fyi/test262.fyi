30b08142b9d6cd7ec449074eac6972f010e421ee
CanadaHonk <oj@oojmed.com>
2023-06-04 20:09:29 +0100

    hermes: fix download and change threads

--- scripts/engines/hermes.sh
+++ scripts/engines/hermes.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 node scripts/install/circle.js facebook/hermes main build linux "output/hermes-cli-linux.tar.gz" hermes.tar.gz
+tar -zxf hermes.tar.gz --directory "hermes"
 
-./scripts/test262.sh hermes "${HOME}/.jsvu/bin/hermes" 64
+./scripts/test262.sh hermes "$PWD/hermes/hermes" 24
