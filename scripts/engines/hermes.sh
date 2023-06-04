c6f04dabebea8be4962cd8d85d035267b1000ac5
CanadaHonk <oj@oojmed.com>
2023-06-04 20:10:13 +0100

    hermes; mkdir extraction dir

--- scripts/engines/hermes.sh
+++ scripts/engines/hermes.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 node scripts/install/circle.js facebook/hermes main build linux "output/hermes-cli-linux.tar.gz" hermes.tar.gz
+
+mkdir hermes
 tar -zxf hermes.tar.gz --directory "hermes"
 
 ./scripts/test262.sh hermes "$PWD/hermes/hermes" 24
