2e804f74817f4b336c659341c2266c6c1642087d
CanadaHonk <oj@oojmed.com>
2023-06-04 17:59:50 +0100

    scripts: use our jsvu fork

--- scripts/install/jsvu.sh
+++ scripts/install/jsvu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# install jsvu as main installer (https://github.com/GoogleChromeLabs/jsvu)
-npm install -g jsvu
+# install jsvu as main installer (our fork - https://github.com/CanadaHonk/jsvu)
+npm install -g github:CanadaHonk/jsvu
 
 # install given engine
 jsvu --os=linux64 --engines=$1
