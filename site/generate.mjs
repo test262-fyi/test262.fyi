2b6a5fc94ffd6ad87b109dbd54c50408412777d7
CanadaHonk <oj@oojmed.com>
2023-06-05 22:44:58 +0100

    generate: make repo names nice

--- site/generate.mjs
+++ site/generate.mjs
@@ -174,6 +174,8 @@ walkStruct(struct);
       if (spec === '404: Not Found' || spec.includes('stage: -1')) spec = await (await fetch(`https://raw.githubusercontent.com/tc39/${repo}/HEAD/README.md`)).text();
 
       info.name = spec.match(/(title:|#) (.*)/i)?.[2];
+      if (info.name.startsWith('proposal-')) info.name = info.name[9].toUpperCase() + info.name.slice(10).replaceAll('-', ' ');
+
       info.stage = parseInt(spec.match(/stage:? ([0-9])/i)?.[1]);
       info.link = `https://github.com/tc39/${repo}`;
 
