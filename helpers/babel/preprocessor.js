28121ca9347e7544d7ca17be3760141c06fe87ae
CanadaHonk <oj@oojmed.com>
2023-06-07 23:47:24 +0100

    babel: fix preproc require path

--- helpers/babel/preprocessor.js
+++ helpers/babel/preprocessor.js
@@ -1,5 +1,5 @@
 // Wrap Babel's test262 runner transpiler to work nicely with test262-harness
-const transpile = require('../babel-test262-runner/lib/run-tests/transpile.js');
+const transpile = require('../../babel-test262-runner/lib/run-tests/transpile.js');
 
 module.exports = function (test) {
   try {
