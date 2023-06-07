2ded87afa777120a38d588391356afc0e39b0ba4
CanadaHonk <oj@oojmed.com>
2023-06-07 23:54:55 +0100

    babel: fix node extract

--- scripts/engines/babel.sh
+++ scripts/engines/babel.sh
@@ -2,10 +2,7 @@
 
 # download node v0.10.x
 curl -L -o node.tar.gz "https://nodejs.org/dist/latest-v0.10.x/node-v0.10.48-linux-x64.tar.gz"
-
-mkdir node
-tar -zxf node.tar.gz -C "node"
-rm node.tar.gz
+tar -zxf node.tar.gz
 
 # download babel/babel-test262-runner as a base (but use our harness instead)
 git clone https://github.com/babel/babel-test262-runner.git --depth 1
@@ -16,4 +13,4 @@ cd ..
 
 node helpers/babel/version.js > version.txt
 
-./scripts/test262.sh node "$(pwd)/node/bin/node" 12 --preprocessor="$(pwd)/helpers/babel/preprocessor.js"
+./scripts/test262.sh node "$(pwd)/node-v0.10.48-linux-x64/bin/node" 12 --preprocessor="$(pwd)/helpers/babel/preprocessor.js"
