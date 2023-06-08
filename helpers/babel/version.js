226622f8731b5082ba3724d8d6764ff1c44afbf5
CanadaHonk <oj@oojmed.com>
2023-06-08 19:55:51 +0100

    babel/version: rewrite

--- helpers/babel/version.js
+++ helpers/babel/version.js
@@ -1,17 +1,7 @@
 const { readFileSync } = require('fs');
 
-const packages = {
-  babel: '@babel/core',
-  'core-js': 'core-js-bundle'
-};
-
 const getPackageVersion = package => JSON.parse(readFileSync(`babel-test262-runner/node_modules/${package}/package.json`), 'utf8').version;
 const shortenVersion = version => version.split('.').slice(0, 2).join('.');
 
-let out = [];
-for (const name in packages) {
-  out.push(`${name} ${shortenVersion(getPackageVersion(packages[name]))}`);
-}
-out.push('node 0.10');
-
-console.log(out.join(' + ').replace('babel ', ''));
+console.log(getPackageVersion('@babel/core'));
+process.write(`(with core-js ${getPackageVersion('core-js-bundle')} on node 0.10.48)`);
