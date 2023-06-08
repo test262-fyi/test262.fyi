3725f86eedf7633d5fb846dc67a701b00b421c46
CanadaHonk <oj@oojmed.com>
2023-06-08 18:45:52 +0100

    babel/transformer: inline core-js

--- helpers/babel/transformer.js
+++ helpers/babel/transformer.js
@@ -1,34 +1,38 @@
 // Jank hacks to inject polyfills :))
 const { resolve } = require('path');
-const coreJs = resolve('../babel-test262-runner/node_modules/core-js-bundle/index.js');
+const { readFileSync } = require('fs');
+
+const coreJsPath = resolve('../babel-test262-runner/node_modules/core-js-bundle/minified.js');
+const coreJsContent = readFileSync(coreJsPath, 'utf8');
 
 // TODO: would inlining these files instead of reads each test be faster?
 const polyfills = `[
   'Function(\\'this.globalThis = this;\\')()',
-  fs.readFileSync('${coreJs.replaceAll('\\', '\\\\')}', 'utf8')
+  ${JSON.stringify(coreJsContent)}
 ]`.replaceAll('\n', '');
 
 module.exports = function (code, pass) {
   if (pass === 1) {
     code = code
-      .replace('preludes: []', `preludes: ${polyfills}`) // run polyfills in new realms
+      .replace('preludes: []', `preludes: polyfills`) // run polyfills in new realms
       .replace('print: print,', `print: print, exports: exports`);
   }
 
   if (pass === 2) {
     code = code
+      .replace('require("vm");', // add polyfills var at the start
+() => `require("vm");
+var polyfills = ${polyfills};
+`)
       .replace('vm.runInESHostContext(', // run polyfills in main
 `
-var fs = require("fs");
-var polyfills = ${polyfills};
 for (var i = 0; i < polyfills.length; i++) {
   vm.runInESHostContext(polyfills[i]);
 }
 
-vm.runInESHostContext(`.replaceAll('\n', ''))
-        .replace('vm.createContext({', `vm.createContext({exports: exports, `);
+vm.runInESHostContext(`)
+        .replace('vm.createContext({', `vm.createContext({exports: exports, polyfills: polyfills,`);
   }
 
-  // console.error(code);
   return code;
 };
