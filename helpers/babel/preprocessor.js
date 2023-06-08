f3c882b4a5a851fc22be86733c47c1338e02e02b
CanadaHonk <oj@oojmed.com>
2023-06-08 14:58:51 +0100

    babel: new debugging

--- helpers/babel/preprocessor.js
+++ helpers/babel/preprocessor.js
@@ -2,7 +2,6 @@
 const transpile = require('../../babel-test262-runner/lib/run-tests/transpile.js');
 
 module.exports = function (test) {
-  console.error('begin preproc', test.file);
   try {
     test.contents = transpile(test.contents, test.attrs.features ?? [], !!test.attrs.flags.module);
   } catch (error) {
@@ -12,7 +11,6 @@ module.exports = function (test) {
       error
     };
   }
-  console.error('finish preproc', test.file);
 
   return test;
 };
