c818d21e230747be9f3a4665f8adf3249fdaa53f
CanadaHonk <oj@oojmed.com>
2023-06-08 12:27:05 +0100

    babel/version: shorten to fit

--- helpers/babel/version.js
+++ helpers/babel/version.js
@@ -6,11 +6,12 @@ const packages = {
 };
 
 const getPackageVersion = package => JSON.parse(readFileSync(`babel-test262-runner/node_modules/${package}/package.json`), 'utf8').version;
+const shortenVersion = version => version.split('.').slice(0, 2).join('.');
 
 let out = [];
 for (const name in packages) {
-  out.push(`${name} ${getPackageVersion(packages[name])}`);
+  out.push(`${name} ${shortenVersion(getPackageVersion(packages[name]))}`);
 }
-out.push('node 0.10.48');
+out.push('node 0.10');
 
-console.log(out.join(' + '));
+console.log(out.join(' + ').replace('babel ', ''));
