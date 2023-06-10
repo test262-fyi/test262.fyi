bc776cb34d9e233852946f74ce7e1a47599c16bd
CanadaHonk <oj@oojmed.com>
2023-06-10 22:55:11 +0100

    chore: adjust thread counts and run all

--- scripts/engines/nashorn.sh
+++ scripts/engines/nashorn.sh
@@ -5,4 +5,4 @@
 
 java -version
 
-./scripts/test262.sh nashorn "$(which jjs)" 4
+./scripts/test262.sh nashorn "$(which jjs)" 8
--- scripts/engines/rhino.sh
+++ scripts/engines/rhino.sh
@@ -3,4 +3,4 @@
 # download rhino (hardcoded link)
 curl -L -o rhino.jar https://github.com/mozilla/rhino/releases/download/Rhino1_7_14_Release/rhino-1.7.14.jar
 
-./scripts/test262.sh rhino "$(pwd)/helpers/rhino.sh" 4
+./scripts/test262.sh rhino "$(pwd)/helpers/rhino.sh" 8
