91a215ea85bd03cef05bbe592ea15dbc7d86a47e
CanadaHonk <oj@oojmed.com>
2023-06-12 21:51:25 +0100

    boa: fix file management

--- scripts/engines/boa.sh
+++ scripts/engines/boa.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# install rust
-curl https://sh.rustup.rs -sSf | sh
-
 # clone repo and build (no CI builds)
 git clone https://github.com/boa-dev/boa.git --depth 1
 
@@ -11,6 +8,9 @@ git rev-parse HEAD > ../version.txt
 
 cargo build --release
 
-cp target/release/boa ../boa
+cp target/release/boa ../boa_bin
+cd ..
+rm -rf boa
+mv boa_bin boa
 
 ./scripts/test262.sh boa "$(pwd)/boa" 24
