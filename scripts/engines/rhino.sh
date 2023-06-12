d62abcbf7f614093c623fcb3bf874da18efe5397
CanadaHonk <oj@oojmed.com>
2023-06-12 17:47:27 +0100

    rhino: add version

--- scripts/engines/rhino.sh
+++ scripts/engines/rhino.sh
@@ -2,5 +2,6 @@
 
 # download rhino (hardcoded link)
 curl -L -o rhino.jar https://github.com/mozilla/rhino/releases/download/Rhino1_7_14_Release/rhino-1.7.14.jar
+echo "1.17.4" > version.txt
 
 ./scripts/test262.sh rhino "$(pwd)/helpers/rhino.sh" 8
