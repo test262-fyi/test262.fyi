26714e269c46db3937b25678646c262628c11225
CanadaHonk <oj@oojmed.com>
2023-06-13 22:19:46 +0100

    sucrase: initial add

--- scripts/engines/nashorn.sh
+++ scripts/engines/nashorn.sh
@@ -3,6 +3,9 @@
 # install java 13
 # sudo apt install openjdk-13-jre-headless
 
+jjs -version
+java -version
+
 nashorn="$(jjs -version <<< "exit()" 2>&1 | head -n 1 | cut -d ' ' -f2)"
 java="$(java -version 2>&1 | head -n 1 | cut -d ' ' -f3 | tr -d '"')"
 
