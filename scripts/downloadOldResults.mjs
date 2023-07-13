db93dcf1ec69151464a05c554af8ddc2108f27b4
Linus Groh <mail@linusgroh.de>
2023-07-13 14:56:33 +0200

    Add support for kiesel

--- scripts/downloadOldResults.mjs
+++ scripts/downloadOldResults.mjs
@@ -1,6 +1,6 @@
 import { writeFileSync, mkdirSync, existsSync } from 'fs';
 
-const engines = [ 'v8', 'sm', 'jsc', 'chakra', 'graaljs', 'hermes', 'libjs', 'engine262', 'qjs', 'xs', 'nashorn', 'rhino', 'babel', 'swc' ];
+const engines = [ 'v8', 'sm', 'jsc', 'chakra', 'graaljs', 'hermes', 'kiesel', 'libjs', 'engine262', 'qjs', 'xs', 'nashorn', 'rhino', 'babel', 'swc' ];
 
 const { artifacts } = await (await fetch('https://api.github.com/repos/CanadaHonk/test262.fyi/actions/artifacts', { headers: { Authorization: 'Bearer ' + process.env.GITHUB_TOKEN }})).json();
 
