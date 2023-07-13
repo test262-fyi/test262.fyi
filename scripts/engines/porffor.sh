76024c80dcf99ab39cdc95f623aef8ff420f6cf2
CanadaHonk <oj@oojmed.com>
2023-07-13 23:50:16 +0100

    porffor: npm install

--- scripts/engines/porffor.sh
+++ scripts/engines/porffor.sh
@@ -5,6 +5,8 @@ git clone https://github.com/CanadaHonk/porffor.git --depth 1
 
 cd porffor
 git rev-parse HEAD > ../version.txt
+
+npm install
 cd ..
 
 ./scripts/test262.sh porffor "$(pwd)/helpers/porffor.sh" 8
