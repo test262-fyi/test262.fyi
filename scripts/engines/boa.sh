8d3e25876a8c564cd83a675837906e3f56f24712
CanadaHonk <oj@oojmed.com>
2023-09-12 00:42:15 +0100

    boa: switch to building ourselves again
    boa ci builds wen?

--- scripts/engines/boa.sh
+++ scripts/engines/boa.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
 # clone repo and build (no CI builds)
-# git clone https://github.com/boa-dev/boa.git --depth 1
+git clone https://github.com/boa-dev/boa.git --depth 1
 
-# cd boa
-# git rev-parse HEAD > ../version.txt
+cd boa
+git rev-parse HEAD > ../version.txt
 
-# cargo build --release
+cargo build --release
 
-# cp target/release/boa ../boa_bin
-# cd ..
-# rm -rf boa
-# mv boa_bin boa
+cp target/release/boa ../boa_bin
+cd ..
+rm -rf boa
+mv boa_bin boa
 
 # download release
-echo "0.17" > version.txt
+# echo "0.17" > version.txt
 
-curl -L -o boa https://github.com/boa-dev/boa/releases/download/v0.17/boa-linux-amd64
-chmod +x boa
+# curl -L -o boa https://github.com/boa-dev/boa/releases/download/v0.17/boa-linux-amd64
+# chmod +x boa
 
 ./scripts/test262.sh boa "$(pwd)/boa" 24
