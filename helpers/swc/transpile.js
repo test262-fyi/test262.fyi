201eaf7fb47150e803c10dbce880e6f36ba855b8
CanadaHonk <honk@goose.icu>
2024-04-08 16:02:51 +0100

    swc: remove ~unused plugin

--- helpers/swc/transpile.js
+++ helpers/swc/transpile.js
@@ -1,15 +1,10 @@
 const swc = require('@swc/core');
-const pluginTransformEval = require('./plugins/transformEval.js');
-const packageJson = require('./package.json');
 
 module.exports = (code, features, mod) => {
   return swc.transformSync(code, {
     isModule: mod,
     sourceMaps: false,
     filename: 'input.js',
-
-    // SWC JS plugins break at >1.3.6 and owner refuses to fix themselves :))
-    plugin: packageJson.dependencies['@swc/core'] === '1.3.6' ? pluginTransformEval : undefined,
 
     jsc: {
       target: 'es5',
