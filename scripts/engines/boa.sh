2c4bc0a40b770987152d84cb70bc22544d095284
Linus Groh <mail@linusgroh.de>
2024-11-09 16:57:15 +0000

    Use boa nightly instead of building from source

--- scripts/engines/boa.sh
+++ scripts/engines/boa.sh
@@ -1,22 +1,14 @@
 #!/bin/sh
 
-# clone repo and build (no CI builds)
+# clone repo to get the git hash
 git clone https://github.com/boa-dev/boa.git --depth 1
 
 cd boa
 git rev-parse HEAD > ../version.txt
-
-cargo build --release --jobs 4 --bin boa
-
-cp target/release/boa ../boa_bin
-cd ..
 rm -rf boa
-mv boa_bin boa
 
 # download release
-# echo "0.17" > version.txt
-
-# curl -L -o boa https://github.com/boa-dev/boa/releases/download/v0.17/boa-linux-amd64
-# chmod +x boa
+curl -L -o boa https://github.com/boa-dev/boa/releases/download/nightly/boa-linux-amd64
+chmod +x boa
 
 ./scripts/test262.sh boa "$(pwd)/boa" 24
