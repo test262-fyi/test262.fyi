5de8d31ac9078c2bd6ff1f738c454ab79eb5a55b
Linus Groh <mail@linusgroh.de>
2024-11-29 19:01:09 +0000

    Update scripts/engines/boa.sh

--- scripts/engines/boa.sh
+++ scripts/engines/boa.sh
@@ -9,8 +9,8 @@ cd ..
 rm -rf boa
 
 # download release
-curl -L -o boa https://github.com/boa-dev/boa/releases/download/nightly/boa-nightly-x86_64-unknown-linux-gnu.tar.gz
-tar -xvf boa
+curl -L -o boa.tar.gz https://github.com/boa-dev/boa/releases/download/nightly/boa-nightly-x86_64-unknown-linux-gnu.tar.gz
+tar -xvf boa.tar.gz
 chmod +x boa
 
 ./scripts/test262.sh boa "$(pwd)/boa" 24
