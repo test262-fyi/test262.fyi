aa70f58be745371b3b76d6d50a4e9eba68ecd4a3
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2025-02-27 19:40:34 +0100

    Fix boa script

--- scripts/engines/boa.sh
+++ scripts/engines/boa.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-# clone repo to get the git hash
-git clone https://github.com/boa-dev/boa.git --depth 1
-
-cd boa
-git rev-parse HEAD > ../version.txt
-cd ..
-rm -rf boa
+URL=https://github.com/boa-dev/boa
+git ls-remote ${URL}.git HEAD | awk '{ print $1 }' > ../version.txt
 
 # download release
-curl -L -o boa.tar.gz https://github.com/boa-dev/boa/releases/download/nightly/boa-nightly-x86_64-unknown-linux-gnu.tar.gz
-tar -xvf boa.tar.gz
+curl -L -o boa ${URL}/releases/download/nightly/boa-x86_64-unknown-linux-gnu
 chmod +x boa
 
 ./scripts/test262.sh boa "$(pwd)/boa" 24
