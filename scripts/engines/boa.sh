b2c85b6abf2ccc94c0b8e092c7aedf482b312f3e
Haled Odat <8566042+HalidOdat@users.noreply.github.com>
2025-03-03 18:34:14 +0100

    Fix boa script host directory

--- scripts/engines/boa.sh
+++ scripts/engines/boa.sh
@@ -7,4 +7,4 @@ git ls-remote ${URL}.git HEAD | cut -f1 > ../version.txt
 curl -L -o boa ${URL}/releases/download/nightly/boa-x86_64-unknown-linux-gnu
 chmod +x boa
 
-./scripts/test262.sh boa "$(pwd)/boa" 24
+./scripts/test262.sh boa "$(pwd)" 24
